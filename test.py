813e2ded68f4ad2387b1b22054932a19e9d1cf23
farrar142 <gksdjf1690@gmail.com>
2021-12-20 00:18:38 +0900

    asd

--- test.py
+++ test.py
@@ -5,6 +5,6 @@ _all = all.split(" ")[0].split("-")[0].split("~")[1].split(".")
 
 print(f"current os version = {all}")
 print(f"{_all[1]},{all[2]}")
-if int(all[1]) < 20:
+if int(_all[1]) < 20:
     if int(_all[2]) <= 4:
         print("you need version more than 23~20.04-1")
