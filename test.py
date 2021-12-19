25dcd71f22e80ddea437b02c406c86c611e39196
farrar142 <gksdjf1690@gmail.com>
2021-12-20 00:18:07 +0900

    asd

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
 import os
 
-test = os.popen("uname -v").read().split(" ")[0].split("-")[0].split("~")[1]
-print(test)
+all = os.popen("uname -v").read()
+_all = all.split(" ")[0].split("-")[0].split("~")[1].split(".")
+
+print(f"current os version = {all}")
+print(f"{_all[1]},{all[2]}")
+if int(all[1]) < 20:
+    if int(_all[2]) <= 4:
+        print("you need version more than 23~20.04-1")
