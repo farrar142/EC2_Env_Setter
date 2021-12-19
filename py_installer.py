8f86d9cba7a8408cc37d6d56fab399bff607abb1
farrar142 <gksdjf1690@gmail.com>
2021-12-19 21:47:49 +0900

    asd

--- py_installer.py
+++ py_installer.py
@@ -17,14 +17,23 @@ def get_logs(cmd):
         exit()
 
 
+def get_cpu_info():
+    if os.name == "nt":
+        return os.popen("WMIC CPU GET NAME")[1].split(" ")[0]
+    elif os.name == "posix":
+        return os.popen("cat /proc/cpuinfo | grep \"model name\"").split(":", 2)[0].trim
+
+
 cmd_list = [
     "sudo apt-get update -y && upgrade -y",
     "sudo apt-get install openjdk-11-jdk",
     "sudo wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -",
     "echo deb http://pkg.jenkins.io/debian-stable binary/  | sudo tee /etc/apt/sources.list.d/jenkins.list",
-    "sudo apt-get install jenkins",
+    "sudo apt-get install jenkins -y",
     "sudo service jenkins restart",
     "sudo apt-get install nginx -y",
+    "sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release",
+    "sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-ketring.gpg",
     "sudo apt-get install docker -y",
     "sudo apt-get install docker-compose -y"
     "sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null",
@@ -34,6 +43,6 @@ cmd_list = [
     "usermod -aG docker jenkins"
 ]
 
-for i in cmd_list:
-    os.system(i)
-port_change()
+# for i in cmd_list:
+#     os.system(i)
+# port_change()
